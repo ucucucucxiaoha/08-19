3cdd3d5bf1ceaf21bc9b31d03eddc75de9f635f5
dahao <1337029494@qq.com>
2018-08-19 23:00:36 +0800

    测试怎么分辨大号，MyTest3提交 22：5

--- src/heihei.java
+++ src/heihei.java
@@ -4,6 +4,7 @@ public class heihei {
         System.out.println("08-19 19：27");
         System.out.println("08-19   大号的 贡献22：29");
         System.out.println("08-19 22:40 xiaohao");
+        System.out.println("08-19   测试怎么分辨大号，MyTest3提交 贡献22：55");
     }
 
 }
